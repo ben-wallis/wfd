757d4a649e6659602c8a2bf27c166543e570cefd
Richard Carson <rscarson@rogers.com>
2025-09-18 15:28:11 -0400

    Add un_options to params to allow overridding settings

--- src/lib.rs
+++ src/lib.rs
@@ -16,18 +16,22 @@ use winapi::{
     shared::{
         minwindef::LPVOID,
         ntdef::LPWSTR,
-        winerror::{HRESULT, SUCCEEDED}
+        winerror::{HRESULT, SUCCEEDED},
     },
-    um:: {
+    um::{
         combaseapi::{CoCreateInstance, CoInitializeEx, CoTaskMemFree, CoUninitialize, CLSCTX_ALL},
         objbase::{COINIT_APARTMENTTHREADED, COINIT_DISABLE_OLE1DDE},
         shobjidl::{IFileDialog, IFileOpenDialog, IFileSaveDialog, IShellItemArray},
-        shobjidl_core::{CLSID_FileOpenDialog, CLSID_FileSaveDialog, IShellItem, SFGAOF, SHCreateItemFromParsingName, SIGDN_FILESYSPATH},
+        shobjidl_core::{
+            CLSID_FileOpenDialog, CLSID_FileSaveDialog, IShellItem, SHCreateItemFromParsingName,
+            SFGAOF, SIGDN_FILESYSPATH,
+        },
         shtypes::COMDLG_FILTERSPEC,
-    }
+    },
 };
 
 // Re-exports
+pub use winapi::shared::windef::HWND;
 pub use winapi::um::shobjidl::{
     FOS_ALLNONSTORAGEITEMS, FOS_ALLOWMULTISELECT, FOS_CREATEPROMPT, FOS_DEFAULTNOMINIMODE,
     FOS_DONTADDTORECENT, FOS_FILEMUSTEXIST, FOS_FORCEFILESYSTEM, FOS_FORCEPREVIEWPANEON,
@@ -36,10 +40,11 @@ pub use winapi::um::shobjidl::{
     FOS_OVERWRITEPROMPT, FOS_PATHMUSTEXIST, FOS_PICKFOLDERS, FOS_SHAREAWARE, FOS_STRICTFILETYPES,
     FOS_SUPPORTSTREAMABLEITEMS,
 };
-pub use winapi::shared::windef::HWND;
 
 macro_rules! com {
-    ($com_expr:expr, $method_name:expr ) => { com(|| unsafe { $com_expr }, $method_name) };
+    ($com_expr:expr, $method_name:expr ) => {
+        com(|| unsafe { $com_expr }, $method_name)
+    };
 }
 
 trait NullTermUTF16 {
@@ -88,6 +93,11 @@ pub struct DialogParams<'a> {
     /// failing to open. Flags should be a combination of `FOS_*` constants, the documentation for
     /// which can be found [here](https://docs.microsoft.com/en-us/windows/win32/api/shobjidl_core/ne-shobjidl_core-_fileopendialogoptions)
     pub options: u32,
+    /// A set of bit flags to REMOVE from the defaults provided by the system. This can be used
+    /// to disable default options such as `FOS_NOVALIDATE` which is enabled by default.
+    /// Flags should be a combination of `FOS_*` constants, the documentation for which can be found
+    /// [here](https://docs.microsoft.com/en-us/windows/win32/api/shobjidl_core/ne-shobjidl_core-_fileopendialogoptions)
+    pub un_options: u32,
     /// The HWND of the window that the dialog will be owned by. If not provided the dialog will be
     /// an independent top-level window.
     pub owner: Option<HWND>,
@@ -96,7 +106,7 @@ pub struct DialogParams<'a> {
     /// containing folder as the initial folder in the dialog.
     pub save_as_item: &'a str,
     /// The text displayed in the title bar of the dialog box
-    pub title: &'a str
+    pub title: &'a str,
 }
 
 impl<'a> Default for DialogParams<'a> {
@@ -111,6 +121,7 @@ impl<'a> Default for DialogParams<'a> {
             folder: "",
             ok_button_label: "",
             options: 0,
+            un_options: 0,
             owner: None,
             save_as_item: "",
             title: "",
@@ -156,7 +167,7 @@ pub enum DialogError {
         /// The COM method that failed
         error_method: String,
         /// The HRESULT error code
-        hresult: i32
+        hresult: i32,
     },
 }
 
@@ -207,20 +218,26 @@ pub enum DialogError {
 /// [`HRESULT`]: https://en.wikipedia.org/wiki/HRESULT
 pub fn open_dialog(params: DialogParams) -> Result<OpenDialogResult, DialogError> {
     // Initialize COM
-    com!(CoInitializeEx(
+    com!(
+        CoInitializeEx(
             null_mut(),
             COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE,
-        ), "CoInitializeEx")?;
+        ),
+        "CoInitializeEx"
+    )?;
 
     // Create IFileOpenDialog instance
     let mut file_open_dialog: *mut IFileOpenDialog = null_mut();
-    com!(CoCreateInstance(
+    com!(
+        CoCreateInstance(
             &CLSID_FileOpenDialog,
             null_mut(),
             CLSCTX_ALL,
             &IFileOpenDialog::uuidof(),
             &mut file_open_dialog as *mut *mut IFileOpenDialog as *mut *mut c_void,
-        ), "CoCreateInstance - IFileOpenDialog")?;
+        ),
+        "CoCreateInstance - IFileOpenDialog"
+    )?;
     let file_open_dialog = unsafe { &*file_open_dialog };
 
     // Perform non open-specific dialog configuration
@@ -231,25 +248,37 @@ pub fn open_dialog(params: DialogParams) -> Result<OpenDialogResult, DialogError
     // Get the item(s) that the user selected in the dialog
     // IFileOpenDialog::GetResults
     let mut shell_item_array: *mut IShellItemArray = null_mut();
-    com!(file_open_dialog.GetResults(&mut shell_item_array), "IFileOpenDialog::GetResults")?;
+    com!(
+        file_open_dialog.GetResults(&mut shell_item_array),
+        "IFileOpenDialog::GetResults"
+    )?;
 
     let shell_item_array = unsafe { &*shell_item_array };
 
     // IShellItemArray::GetCount
     let mut item_count: u32 = 0;
-    com!(shell_item_array.GetCount(&mut item_count), "IShellItemArray::GetCount")?;
+    com!(
+        shell_item_array.GetCount(&mut item_count),
+        "IShellItemArray::GetCount"
+    )?;
 
     let mut file_paths: Vec<PathBuf> = vec![];
     for i in 0..item_count {
         // IShellItemArray::GetItemAt
         let mut shell_item: *mut IShellItem = null_mut();
-        com!(shell_item_array.GetItemAt(i, &mut shell_item), "IShellItemArray::GetItemAt")?;
+        com!(
+            shell_item_array.GetItemAt(i, &mut shell_item),
+            "IShellItemArray::GetItemAt"
+        )?;
         let shell_item = unsafe { &*shell_item };
 
         // Fetch the SFGAO_FILESYSTEM attribute for the file
         let mut attribs: SFGAOF = 0;
         // IShellItem::GetAttributes
-        com!(shell_item.GetAttributes(SFGAO_FILESYSTEM, &mut attribs), "IShellItem::GetAttributes")?;
+        com!(
+            shell_item.GetAttributes(SFGAO_FILESYSTEM, &mut attribs),
+            "IShellItem::GetAttributes"
+        )?;
 
         // Ignore shell items that do not have the SFGAO_FILESYSTEM attribute
         // which indicates that they represent a valid path to a file or folder
@@ -272,13 +301,15 @@ pub fn open_dialog(params: DialogParams) -> Result<OpenDialogResult, DialogError
         CoUninitialize();
     }
 
-    file_paths.get(0).cloned().map(|x| {
-        OpenDialogResult {
+    file_paths
+        .get(0)
+        .cloned()
+        .map(|x| OpenDialogResult {
             selected_file_path: x,
             selected_file_paths: file_paths,
-            selected_file_type_index: selected_filter_index
-        }
-    }).ok_or(DialogError::UnsupportedFilepath)
+            selected_file_type_index: selected_filter_index,
+        })
+        .ok_or(DialogError::UnsupportedFilepath)
 }
 
 /// Displays a Save Dialog using the provided parameters.
@@ -315,31 +346,46 @@ pub fn open_dialog(params: DialogParams) -> Result<OpenDialogResult, DialogError
 /// [`HRESULT`]: https://en.wikipedia.org/wiki/HRESULT
 pub fn save_dialog(params: DialogParams) -> Result<SaveDialogResult, DialogError> {
     // Initialize COM
-    com!(CoInitializeEx(
+    com!(
+        CoInitializeEx(
             null_mut(),
             COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE,
-        )
-    , "CoInitializeEx")?;
+        ),
+        "CoInitializeEx"
+    )?;
 
     // Create IFileSaveDialog instance
     let mut file_save_dialog: *mut IFileSaveDialog;
     file_save_dialog = null_mut();
-    com!(CoCreateInstance(
+    com!(
+        CoCreateInstance(
             &CLSID_FileSaveDialog,
             null_mut(),
             CLSCTX_ALL,
             &IFileSaveDialog::uuidof(),
             &mut file_save_dialog as *mut *mut IFileSaveDialog as *mut *mut c_void,
-        )
-    , "CoCreateInstance - FileSaveDialog")?;
+        ),
+        "CoCreateInstance - FileSaveDialog"
+    )?;
     let file_save_dialog = unsafe { &*file_save_dialog };
 
     // IFileDialog::SetSaveAsItem
     if params.save_as_item != "" {
         let mut item: *mut IShellItem = null_mut();
         let path = params.save_as_item.as_null_term_utf16();
-        com!(SHCreateItemFromParsingName(path.as_ptr(), null_mut(), &IShellItem::uuidof(), &mut item as *mut *mut IShellItem as *mut *mut c_void), "SHCreateItemFromParsingName")?;
-        com!(file_save_dialog.SetSaveAsItem(item), "IFileDialog::SetSaveAsItem")?;
+        com!(
+            SHCreateItemFromParsingName(
+                path.as_ptr(),
+                null_mut(),
+                &IShellItem::uuidof(),
+                &mut item as *mut *mut IShellItem as *mut *mut c_void
+            ),
+            "SHCreateItemFromParsingName"
+        )?;
+        com!(
+            file_save_dialog.SetSaveAsItem(item),
+            "IFileDialog::SetSaveAsItem"
+        )?;
         unsafe {
             let item = &*item;
             item.Release();
@@ -353,7 +399,10 @@ pub fn save_dialog(params: DialogParams) -> Result<SaveDialogResult, DialogError
 
     // IFileDialog::GetResult
     let mut shell_item: *mut IShellItem = null_mut();
-    com!(file_save_dialog.GetResult(&mut shell_item), "IFileDialog::GetResult")?;
+    com!(
+        file_save_dialog.GetResult(&mut shell_item),
+        "IFileDialog::GetResult"
+    )?;
     let shell_item = unsafe { &*shell_item };
     let file_name = get_shell_item_display_name(&shell_item)?;
     unsafe { shell_item.Release() };
@@ -397,19 +446,36 @@ fn show_dialog(file_dialog: &IFileDialog, owner: Option<HWND>) -> Result<(), Dia
     }
 }
 
-fn configure_file_dialog(file_dialog: &IFileDialog, params: &DialogParams) -> Result<(), DialogError> {
+fn configure_file_dialog(
+    file_dialog: &IFileDialog,
+    params: &DialogParams,
+) -> Result<(), DialogError> {
     // IFileDialog::SetDefaultExtension
     if params.default_extension != "" {
         let default_extension = params.default_extension.as_null_term_utf16();
-        com!(file_dialog.SetDefaultExtension(default_extension.as_ptr()), "IFileDialog::SetDefaultExtension")?;
+        com!(
+            file_dialog.SetDefaultExtension(default_extension.as_ptr()),
+            "IFileDialog::SetDefaultExtension"
+        )?;
     }
 
     // IFileDialog::SetDefaultFolder
     if params.default_folder != "" {
         let mut default_folder: *mut IShellItem = null_mut();
         let path = params.default_folder.as_null_term_utf16();
-        com!(SHCreateItemFromParsingName(path.as_ptr(), null_mut(), &IShellItem::uuidof(), &mut default_folder as *mut *mut IShellItem as *mut *mut c_void), "SHCreateItemFromParsingName")?;
-        com!(file_dialog.SetDefaultFolder(default_folder), "IFileDialog::SetDefaultFolder")?;
+        com!(
+            SHCreateItemFromParsingName(
+                path.as_ptr(),
+                null_mut(),
+                &IShellItem::uuidof(),
+                &mut default_folder as *mut *mut IShellItem as *mut *mut c_void
+            ),
+            "SHCreateItemFromParsingName"
+        )?;
+        com!(
+            file_dialog.SetDefaultFolder(default_folder),
+            "IFileDialog::SetDefaultFolder"
+        )?;
         unsafe {
             let default_folder = &*default_folder;
             default_folder.Release();
@@ -420,7 +486,15 @@ fn configure_file_dialog(file_dialog: &IFileDialog, params: &DialogParams) -> Re
     if params.folder != "" {
         let mut folder: *mut IShellItem = null_mut();
         let path = params.folder.as_null_term_utf16();
-        com!(SHCreateItemFromParsingName(path.as_ptr(), null_mut(), &IShellItem::uuidof(), &mut folder as *mut *mut IShellItem as *mut *mut c_void), "SHCreateItemFromParsingName")?;
+        com!(
+            SHCreateItemFromParsingName(
+                path.as_ptr(),
+                null_mut(),
+                &IShellItem::uuidof(),
+                &mut folder as *mut *mut IShellItem as *mut *mut c_void
+            ),
+            "SHCreateItemFromParsingName"
+        )?;
         com!(file_dialog.SetFolder(folder), "IFileDialog::SetFolder")?;
         unsafe {
             let folder = &*folder;
@@ -431,13 +505,19 @@ fn configure_file_dialog(file_dialog: &IFileDialog, params: &DialogParams) -> Re
     // IFileDialog::SetFileName
     if params.file_name != "" {
         let initial_file_name = params.file_name.as_null_term_utf16();
-        com!(file_dialog.SetFileName(initial_file_name.as_ptr()), "IFileDialog::SetFileName")?;
+        com!(
+            file_dialog.SetFileName(initial_file_name.as_ptr()),
+            "IFileDialog::SetFileName"
+        )?;
     }
 
     // IFileDialog::SetFileNameLabel
     if params.file_name_label != "" {
         let file_name_label = params.file_name_label.as_null_term_utf16();
-        com!(file_dialog.SetFileNameLabel(file_name_label.as_ptr()), "IFileDialog::SetFileNameLabel")?;
+        com!(
+            file_dialog.SetFileNameLabel(file_name_label.as_ptr()),
+            "IFileDialog::SetFileNameLabel"
+        )?;
     }
 
     if !params.file_types.is_empty() {
@@ -446,34 +526,55 @@ fn configure_file_dialog(file_dialog: &IFileDialog, params: &DialogParams) -> Re
 
     // IFileDialog::SetFileTypeIndex
     if !params.file_types.is_empty() && params.file_type_index > 0 {
-        com!(file_dialog.SetFileTypeIndex(params.file_type_index), "IFileDialog::SetFileTypeIndex")?;
+        com!(
+            file_dialog.SetFileTypeIndex(params.file_type_index),
+            "IFileDialog::SetFileTypeIndex"
+        )?;
     }
 
     // IFileDialog::SetOkButtonLabel
     if params.ok_button_label != "" {
         let ok_buttom_label = params.ok_button_label.as_null_term_utf16();
-        com!(file_dialog.SetOkButtonLabel(ok_buttom_label.as_ptr()), "IFileDialog::SetOkButtonLabel")?;
+        com!(
+            file_dialog.SetOkButtonLabel(ok_buttom_label.as_ptr()),
+            "IFileDialog::SetOkButtonLabel"
+        )?;
     }
 
-    if params.options > 0 {
+    if params.options > 0 || params.un_options > 0 {
         // IFileDialog::GetOptions
         let mut existing_options: u32 = 0;
-        com!(file_dialog.GetOptions(&mut existing_options), "IFileDialog::GetOptions")?;
+        com!(
+            file_dialog.GetOptions(&mut existing_options),
+            "IFileDialog::GetOptions"
+        )?;
+
+        existing_options &= !params.un_options;
+        existing_options |= params.options;
 
         // IFileDialog::SetOptions
-        com!(file_dialog.SetOptions(existing_options | params.options), "IFileDialog::SetOptions")?;
+        com!(
+            file_dialog.SetOptions(existing_options),
+            "IFileDialog::SetOptions"
+        )?;
     }
 
     // IFileDialog::SetTitle
     if params.title != "" {
         let title = params.title.as_null_term_utf16();
-        com!(file_dialog.SetTitle(title.as_ptr()), "IFileDialog::SetTitle")?;
+        com!(
+            file_dialog.SetTitle(title.as_ptr()),
+            "IFileDialog::SetTitle"
+        )?;
     }
 
     Ok(())
 }
 
-fn add_filters(dialog: &IFileDialog, filters: &[FileExtensionFilterPair]) -> Result<(), DialogError> {
+fn add_filters(
+    dialog: &IFileDialog,
+    filters: &[FileExtensionFilterPair],
+) -> Result<(), DialogError> {
     // Create a vec holding the UTF-16 string pairs for the filter - we need
     // to have these in a vec since we need to be able to pass a pointer to them
     // in the COMDLG_FILTERSPEC structs passed to SetFileTypes.
@@ -495,7 +596,10 @@ fn add_filters(dialog: &IFileDialog, filters: &[FileExtensionFilterPair]) -> Res
         .collect::<Vec<COMDLG_FILTERSPEC>>();
 
     // IFileDialog::SetFileTypes
-    com!(dialog.SetFileTypes(filter_specs.len() as u32, filter_specs.as_ptr()), "IFileDialog::SetFileTypes")?;
+    com!(
+        dialog.SetFileTypes(filter_specs.len() as u32, filter_specs.as_ptr()),
+        "IFileDialog::SetFileTypes"
+    )?;
 
     Ok(())
 }
@@ -503,14 +607,20 @@ fn add_filters(dialog: &IFileDialog, filters: &[FileExtensionFilterPair]) -> Res
 fn get_file_type_index(file_dialog: &IFileDialog) -> Result<u32, DialogError> {
     // IFileDialog::GetFileTypeIndex
     let mut selected_filter_index: u32 = 0;
-    com!(file_dialog.GetFileTypeIndex(&mut selected_filter_index), "IFileDialog::GetFileTypeIndex")?;
+    com!(
+        file_dialog.GetFileTypeIndex(&mut selected_filter_index),
+        "IFileDialog::GetFileTypeIndex"
+    )?;
     Ok(selected_filter_index)
 }
 
 fn get_shell_item_display_name(shell_item: &IShellItem) -> Result<OsString, DialogError> {
     let mut display_name: LPWSTR = null_mut();
     // IShellItem::GetDisplayName
-    com!(shell_item.GetDisplayName(SIGDN_FILESYSPATH, &mut display_name), "IShellItem::GetDisplayName")?;
+    com!(
+        shell_item.GetDisplayName(SIGDN_FILESYSPATH, &mut display_name),
+        "IShellItem::GetDisplayName"
+    )?;
     let slice = unsafe { slice::from_raw_parts(display_name, wcslen(display_name)) };
     let result = OsString::from_wide(slice);
 
@@ -530,7 +640,8 @@ where
     if !SUCCEEDED(hresult) {
         Err(DialogError::HResultFailed {
             hresult,
-            error_method: method.to_string() })
+            error_method: method.to_string(),
+        })
     } else {
         Ok(())
     }
